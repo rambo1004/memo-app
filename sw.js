52f3d0f62e2eddb85c097f261e5b2c3d4f0480c7
Lee Cham sin <rambo.work365@gmail.com>
2026-09-16 22:24:56 +0900

    Initial commit: Add memo app files

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'planner-pro-v1';
+const ASSETS = [
+    './memo.html',
+    './manifest.json',
+    './icon-192.jpg',
+    './icon-512.jpg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
